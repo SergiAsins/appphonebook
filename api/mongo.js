30c68ab90c990954e1c0b76050f9bbe65546bd65
SergiAsins <sergi.prof@hotmail.com>
2024-07-16 18:19:54 +0200

    last update

--- api/mongo.js
+++ api/mongo.js
@@ -1,9 +1,9 @@
-// This file enables the connection with MongoDB and CRUD operations
-const mongoose = require('mongoose');
+// This file enables the connection with MongoDB and the CRUD operations
+import { connect, Schema, model, connection } from 'mongoose';
 
-// Verifies if the password was given
+// Verifies if the password was provided
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>');
+    console.log('please provide the password as an argument: node mongo.js <password>');
     process.exit(1);
 }
 
@@ -11,36 +11,35 @@ const password = process.argv[2];
 const name = process.argv[3];
 const number = process.argv[4];
 
-const url = `mongodb+srv://HasanAsins:${password}@clusterasinshasan.yko1cvx.mongodb.net/phonebook?retryWrites=true&w=majority&appName=ClusterAsinsHasan`;
+const url = `mongodb+srv://HasanAsins:${password}@clusterasinshasan.yko1cvx.mongodb.net/phonebook?retryWrites=true&w=majority`;
+connect(url);
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const personSchema = new mongoose.Schema({
+const personSchema = new Schema({
     name: String,
     number: String,
 });
 
-const Person = mongoose.model('Person', personSchema);
+const Person = model('Person', personSchema);
 
-// If only the password is given, it shows all the entries
+// If only the password is provided, it shows all the entries
 if (process.argv.length === 3) {
     Person.find({}).then(result => {
-        console.log('Phonebook:');
+        console.log('phonebook');
         result.forEach(person => {
             console.log(`${person.name} ${person.number}`);
         });
-        mongoose.connection.close();
+        connection.close();
     });
 } else if (process.argv.length === 5) {
-    // If a name and a number are given, it adds a new entry
+    // If a name and number are provided, it adds a new entry
     const person = new Person({
         name: name,
         number: number,
     });
 
     person.save().then(() => {
-        console.log(`Added ${name} number ${number} to the phonebook`);
-        mongoose.connection.close();
+        console.log(`added ${name} number ${number} to the phonebook`);
+        connection.close();
     });
 } else {
     console.log('Please provide the correct number of arguments: node mongo.js <password> <name> <number>');
