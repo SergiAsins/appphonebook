d17594e3888e42900076f3f2478516ea3afea832
SergiAsins <sergi.prof@hotmail.com>
2024-07-22 12:03:22 +0200

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html'  // Asegúrate de que este sea el camino correcto
+    }
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'https://service-appphonebook.onrender.com/api/persons',
+        target: 'https://service-appphonebook.onrender.com',
         changeOrigin: true,
       },
-    }
+    },
   },
 })
